073f507b12eaf02fdc096ae85433d52180655933
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-07-30 01:11:12 -0700

    Creando los modelos Contacto y Usuario

---
+++ prueba_tecnica_backend/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace prueba_tecnica_backend.Models
+{
+    public class Contact
+    {
+        [Key]
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Phone { get; set; }
+        public required string Email { get; set; }
+        public required string References { get; set; }
+
+    }
+}
---
+++ prueba_tecnica_backend/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace prueba_tecnica_backend.Models
+{
+    public class User
+    {
+        [Key]
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required DateTime CreateDate { get; set; }
+    }
+}
