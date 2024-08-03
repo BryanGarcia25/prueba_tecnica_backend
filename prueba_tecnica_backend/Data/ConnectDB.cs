3c22444dee374470203c02f3dc4232f874e0b23b
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-08-03 02:11:44 -0700

    Creando clase para realizar la conexión con la base de datos MySQL

---
+++ prueba_tecnica_backend/Data/ConnectDB.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using prueba_tecnica_backend.Models;
+
+namespace prueba_tecnica_backend.Data
+{
+    public class ConnectDB : DbContext
+    {
+        private IConfiguration _configuration;
+        public DbSet<User> Users { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+
+        public ConnectDB(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var serverVersion = ServerVersion.AutoDetect(_configuration["CONNECT_DB"]);
+            optionsBuilder.UseMySql(_configuration["CONNECT_DB"], serverVersion);
+        }
+    }
+}
