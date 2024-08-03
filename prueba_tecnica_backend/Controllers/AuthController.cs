c305c6684e9c4c50c0c6e6a5c2de55b38f7308f2
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-08-03 02:18:38 -0700

    Generando el token JWT para la autenticación y autorización de los usuarios

--- prueba_tecnica_backend/Controllers/AuthController.cs
+++ prueba_tecnica_backend/Controllers/AuthController.cs
@@ -1,6 +1,38 @@
-﻿namespace prueba_tecnica_backend.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using prueba_tecnica_backend.Data;
+using prueba_tecnica_backend.Models;
+using prueba_tecnica_backend.Utils;
+using System.Text;
+
+namespace prueba_tecnica_backend.Controllers
 {
-    public class AuthController
+    [ApiController]
+    public class AuthController : ControllerBase
     {
+        private readonly ConnectDB _connectDB;
+        private readonly GenerateJWTToken _generateJWTToken;
+
+        public AuthController(ConnectDB connectDB, GenerateJWTToken generateJWTToken) 
+        {
+            _connectDB = connectDB;
+            _generateJWTToken = generateJWTToken;
+        }
+
+        [HttpPost]
+        [Route("auth/login")]
+        public async Task<IActionResult> getUsername(Authentication authentication)
+        {
+            var registeredUser = await _connectDB.Users.Where(x => x.Name == authentication.Name && x.Email == authentication.Email).FirstOrDefaultAsync();
+
+            if (registeredUser != null)
+            {
+                return Ok(_generateJWTToken.GenerateJWT(registeredUser, _generateJWTToken.Get_configuration()));                
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
     }
 }
---
+++ prueba_tecnica_backend/Utils/GenerateJWTToken.cs
@@ -0,0 +1,45 @@
+﻿using prueba_tecnica_backend.Models;
+using System.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace prueba_tecnica_backend.Utils
+{
+    public class GenerateJWTToken
+    {
+        private IConfiguration _configuration;
+        public GenerateJWTToken(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+
+        public IConfiguration Get_configuration()
+        {
+            return _configuration;
+        }
+
+        public string GenerateJWT(User user, IConfiguration _configuration)
+        {
+            var userClaims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
+
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT_TOKEN"]!));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            var jwtConfiguration = new JwtSecurityToken(
+                claims: userClaims,
+                expires: DateTime.UtcNow.AddMinutes(30),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(jwtConfiguration);
+        }
+    }
+}
