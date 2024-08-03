6ebf6a39225d87d1d6f9bcd003f0e67f62d4f4f3
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-08-03 02:09:31 -0700

    Creando clase Authentication para guardar el nombre y correo del usuario que ingresa desde el frontend

---
+++ prueba_tecnica_backend/Models/Authentication.cs
@@ -0,0 +1,8 @@
+﻿namespace prueba_tecnica_backend.Models
+{
+    public class Authentication
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+    }
+}
