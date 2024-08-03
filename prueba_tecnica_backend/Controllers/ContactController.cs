a8aad9b7e8eae932d038cb2cd4045fb86da79f5d
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-08-03 02:17:37 -0700

    Generando el CRUD de los contactos para la base de datos con su ruta asignada

--- prueba_tecnica_backend/Controllers/ContactController.cs
+++ prueba_tecnica_backend/Controllers/ContactController.cs
@@ -1,43 +1,76 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using prueba_tecnica_backend.Data;
+using prueba_tecnica_backend.Models;
 
 namespace prueba_tecnica_backend.Controllers
 {
     [ApiController]
     public class ContactController : ControllerBase
     {
+        private readonly ConnectDB _connectDB;
+        public ContactController(ConnectDB connectDB) 
+        {
+            _connectDB = connectDB;
+        }
+
         [HttpPost]
         [Route("contact/create")]
-        public string CreateContact()
+        public async Task<IActionResult> CreateContact(Contact contact)
         {
-            return "Creando contacto nuevo";
+            _connectDB.Contacts.Add(contact);
+            await _connectDB.SaveChangesAsync();
+            return Ok(contact);
         }
 
         [HttpGet]
         [Route("contact/registered_contacts")]
-        public string GetContacts()
+        public async Task<IActionResult> GetContacts()
         {
-            return "Obteniendo contactos registrados";
+            var registeredContacts = await _connectDB.Contacts.ToListAsync();
+            return Ok(registeredContacts);
         }
 
         [HttpGet]
         [Route("contact/registered_contact/{id}")]
-        public string GetContactById(int id)
+        public IActionResult GetContactById(int id)
         {
-            return "Obteniendo contacto con el id " + id;
+            var registeredContact = _connectDB.Contacts.Find(id);
+
+            if (registeredContact == null) 
+            {
+                return NotFound();
+            } else
+            {
+                return Ok(registeredContact);
+            }
+
         }
 
         [HttpPost]
-        [Route("contact/update/{id}")]
-        public string UpdateContactById(int id)
+        [Route("contact/update")]
+        public async Task<IActionResult> UpdateContactById(Contact contact)
         {
-            return "Actualizando contacto con el id " + id;
+            _connectDB.Contacts.Update(contact);
+            await _connectDB.SaveChangesAsync();
+            return Ok(contact);
         }
 
         [HttpPost]
         [Route("contact/delete{id}")]
-        public string DeleteContactById(int id)
+        public async Task<IActionResult> DeleteContactById(int id)
         {
-            return "Eliminando contacto con el id " + id;
+            var registerContact = _connectDB.Contacts.Find(id);
+            if (registerContact == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _connectDB.Contacts.Remove(registerContact);
+                await _connectDB.SaveChangesAsync();
+                return Ok("Se ha eliminado el contacto con el id " + id);
+            }
         }
     }
 }
